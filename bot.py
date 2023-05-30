2722bbc407d06d15c34ad06f4f935047020e9faf
avojarot <114910982+Majarot@users.noreply.github.com>
2023-05-30 20:11:27 +0300

    try catch fixed

--- bot.py
+++ bot.py
@@ -17,7 +17,14 @@ dispecher = Dispatcher(bot)
 
 def convert_speech_to_text(audio_filepath):
     with open(audio_filepath, "rb") as audio:
-        transcript = openai.Audio.transcribe("whisper-1", audio)
+        is_not_done = True
+        while is_not_done:
+            try:
+                transcript = openai.Audio.transcribe("whisper-1", audio)
+                is_not_done = False
+            except Exception:
+                is_not_done = True
+
         return transcript["text"]
 
 
@@ -33,14 +40,13 @@ async def asr_in_voice(message: types.Message):
     voice = message.voice if message.voice else message.audio
     voice_file = await voice.get_file()
     file_format = voice_file.file_path.split(".")[-1]
-    print(file_format)
 
     ogg_file = await download_voice(voice_file, bot, file_format)
     mp3_file = convert_to_mp3(ogg_file, file_format)
 
     text = convert_speech_to_text(mp3_file)
     os.remove(ogg_file)
-    # os.remove(mp3_file)
+    os.remove(mp3_file)
     await message.answer(text)
 
 
